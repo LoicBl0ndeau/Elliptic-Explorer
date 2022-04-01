b82c2a76af3409cfc6869c994948ca3cce1ee8f3
0xtter <41399587+0xtter@users.noreply.github.com>
2022-04-01 16:29:05 +0200

    added menu again

---
+++ src/components/menu/MenuParametreMont.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Montgomery">
+    <form
+      id="form"
+      @submit="checkForm"
+      action="/something2"
+      method="post"
+      novalidate="true"
+    >
+      <p>
+        <input v-model="a_param" placeholder="a"/><br />
+        <input v-model="b_param" placeholder="b"/><br />
+        <input v-model="module_param" placeholder="modulo"/><br />
+      </p>
+
+      <p>
+        <input type="submit" value="Générer la courbe" />
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuParametreMont",
+  props: {
+    msg2: String,
+    a: Number,
+    b: Number,
+    modulo: Number
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/menu/MenuParametreShort.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="weirstrass">
+      <p>
+        <input id="a" v-model="a_param" placeholder="a" onclick="RecupA" /><br/>
+        <input id="b" v-model="b_param" placeholder="b"  onclick="RecupB" /><br/>
+        <input id="modulo" v-model="module_param" placeholder="modulo"  onclick="RecupModulo" /><br/>
+      </p>
+
+      <p>
+        <input type="submit" value="Générer la courbe" @click="param" />
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuParametreShort",
+  props: {
+    msg1: String,
+    a: Number,
+    b: Number,
+    modulo: Number
+  },
+  methods: {
+    param() {
+      this.parametres = !this.parametres;
+    },
+    getA() {
+      return document.getElementById("a").value;
+    },
+    getB() {
+      return document.getElementById("b").value;
+    },
+    getP() {
+      return document.getElementById("modulo").value;
+    },
+  },
+  data: function() {
+    return {
+      a_param: [],
+      parametres: false,
+      valeurA:'',
+      valeurB:'',
+      valeurP:''
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
