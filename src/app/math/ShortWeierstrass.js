65c845718aab4e47600f22361d646c719f1b4abe
Laura25082000 <71756357+Laura25082000@users.noreply.github.com>
2022-04-19 09:32:23 +0200

    Update ShortWeierstrass.js

--- src/app/math/ShortWeierstrass.js
+++ src/app/math/ShortWeierstrass.js
@@ -189,7 +189,7 @@ export class ShortWeierstrass extends ModCurveGraph {
         let listPoints = this.listPoints;
         let listCoordPoints = this.listCoordPoints;
         listPoints.forEach(item => {
-            listCoordPoints.push(getCoord(item));
+            listCoordPoints.push(this.getCoord(item));
         });
     }
 
@@ -214,15 +214,21 @@ export class ShortWeierstrass extends ModCurveGraph {
      */
     displayModulo(){
         var modulo=this.param.p;
+        var lignes =5;
         try {
             this.calculator.setExpressions([
                 { id: `m`, latex: `m=${modulo}` },
-            ]);
-            this.calculator.setExpressions([
+                { id: `l`, latex: `l=${lignes}` },
+                { id: `L`, latex: `L=[\\frac{m}{2}i\\operatorname{for}i=[\\operatorname{floor}(-\\frac{lm}{2})...\\operatorname{floor}(\\frac{lm}{2})]]` },
                 { id: `a`, latex: `a=(y_{${this.idSelectedPoints[1]}}-y_{${this.idSelectedPoints[0]}})`},
                 { id: `b`, latex: `b=(x_{${this.idSelectedPoints[0]}}-x_{${this.idSelectedPoints[1]}})`},
                 { id: `d`, latex: `d=(x_{${this.idSelectedPoints[0]}}y_{${this.idSelectedPoints[1]}}-x_{${this.idSelectedPoints[1]}}y_{${this.idSelectedPoints[0]}})`},
-                { id: `e`, latex: `\\operatorname{mod}\\left(ax+by,m\\right)\\ =\\operatorname{mod}\\left(d,m\\right)\\ \\left\\{0<x<m\\right\\}\\ \\left\\{0<y<m\\right\\}`, color: Graphic.Colors.curve },
+                { id: `c`, latex: `c=((x_${this.idSelectedPoints[0]}+L)y_${this.idSelectedPoints[1]}-(x_${this.idSelectedPoints[1]}+L)y_${this.idSelectedPoints[0]})`},
+            ]);
+            
+            this.calculator.setExpressions([
+                { id: `e`, latex: `\\operatorname{mod}\\left(ax+by,m\\right)\\ =\\operatorname{mod}\\left(d,m\\right)\\ \\left\\{-0.5<x<m-0.5\\right\\}\\ \\left\\{-0.5<y<m-0.5\\right\\}`, color: Graphic.Colors.curve },
+                { id: `f`, latex: `(ax+by)=c \\left\\{0<x<m\\right\\}\\ \\left\\{0<y<m\\right\\}`, color: Graphic.Colors.line },
             ]);
         } catch (error) {
             throw new Error(`An error has occured adding modular lines : ${error}`);
@@ -237,7 +243,7 @@ export class ShortWeierstrass extends ModCurveGraph {
     displayAddPoint(addPoint, isTheSamePoint){
         var that = this;
         let listPoints = that.listPoints;
-        let negPoint = getCoord(that.newPoint(addPoint[0],addPoint[1]).neg());
+        let negPoint = that.getCoord(that.newPoint(addPoint[0],addPoint[1]).neg());
         var i=1;
         var j=1;
         for (i=1; i<listPoints.length ; i++ ){
