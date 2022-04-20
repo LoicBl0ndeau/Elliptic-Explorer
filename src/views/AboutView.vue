3e743aa425fa23b9fe4c33baaf57d1941d0fae5b
0xtter <41399587+0xtter@users.noreply.github.com>
2022-04-20 14:51:31 +0200

    Revert "Merge branch 'main' of https://github.com/DanielArian/elliptic-explorer"
    This reverts commit 7daa6b1bc99f7cda76675719fec4e1502aab1b38, reversing
    changes made to b3fcd9801b5a3ade1dc71e2c82a1e840c4f38c6b.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,6 +5,9 @@
     <!-- <button @click='open("anglais")'>Anglais</button> -->
     <AccueilAnglais />
   </div>
+  <center>
+    <img src="https://centre.hei.fr/wp-content/uploads/2020/10/junia.jpg" width="700" height="150"/>
+  </center>
   <div>
     <MyMenu />
   </div>
@@ -43,4 +46,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+img {
+  margin-left: 28%;
+}
+</style>
 
