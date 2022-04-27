b5de131ba4de094daaa70161bf87720929240121
DanielArian <71820558+DanielArian@users.noreply.github.com>
2022-04-27 10:01:54 +0200

    Update menu.js
    + comments

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -6,24 +6,53 @@ import katex from 'katex';
 export const menuStore = defineStore('menu', {
     actions: {
         /**
-         * @param {string} htmlID l'id de l'élémeent HTML
-         * @returns sa valeur
+         * Sets hmtl element display style to "inline"
+         * 
+         * @param {string} htmlID id of the HTML element
          */
         displayElementById(htmlID) {
             document.getElementById(htmlID).style.display = "inline";
         },
+        /**
+         * Sets hmtl element display style to "none"
+         * 
+         * @param {string} htmlID id of the HTML element
+         */
         hideElementById(htmlID) {
             document.getElementById(htmlID).style.display = "none";
         },
+        /**
+         * Sets hmtl element display style to "inline"
+         * 
+         * @param {string} htmlID id of the HTML element
+         */
         setValueById(htmlID, value) {
             document.getElementById(htmlID).value = value;
         },
+        /**
+         * Gets the value of the element
+         * 
+         * @param {string} htmlID id of the HTML element
+         * @return {string} element.value
+         */
         getValueById(htmlID) {
             return document.getElementById(htmlID).value;
         },
+        /**
+         * Gets the value of an input and parse it into a integer.
+         * 
+         * @param {string} htmlID id of the HTML element
+         * @return integer or NaN
+         */
         getIntFromInputId(htmlID) {
             return Number.parseInt(this.getValueById(htmlID));
         },
+        /**
+         * Gets the value of an input and parse it into a float.
+         * 
+         * @param {string} htmlID id of the HTML element
+         * @return float or NaN
+         */
         getFloatFromInputId(htmlID) {
             return Number.parseFloat(this.getValueById(htmlID));
         },
@@ -38,6 +67,13 @@ export const menuStore = defineStore('menu', {
             let value = Number.parseFloat(this.getValueById(inputHtmlID));
             graphS.setExpValue(graphExp, value);
         },
+        /**
+         * Fill an input field with a value from the expressions list 
+         * in DesmosAPI.
+         * 
+         * @param {string} graphExp expression in the expression list (e.g "x_{1}")
+         * @param {*} inputHtmlID id of the input field
+         */
         setInputValueFromGraphExpValue(htmlID, exp) {
             const graphS = graphStore();
 
@@ -48,6 +84,7 @@ export const menuStore = defineStore('menu', {
         },
         /**
          * Replace innerHTML value of an element with rendered latex expression
+         * 
          * @param {string} htmlID the id of element on which we want to use to display LaTex in its innerHTML
          * @param {string} latexExp LaTeX expression
          */
