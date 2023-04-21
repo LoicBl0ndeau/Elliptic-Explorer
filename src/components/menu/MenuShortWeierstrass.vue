580e3ae303b91c36a87af208dbe251421a8d4cbc
Sheron17 <sheron.millien@student.junia.com>
2023-04-21 13:47:35 +0200

    updateCurveDisplay

--- src/components/menu/MenuShortWeierstrass.vue
+++ src/components/menu/MenuShortWeierstrass.vue
@@ -165,6 +165,7 @@ export default {
       let displayValue = this.controleur.getCorps() == "Modulo" ? "block" : "none";
 
       document.getElementById('p_container_ShortWeierstrass').style.display = displayValue;
+      document.getElementById('updateCurveDisplay').style.display = displayValue;
     },
     updateLatexDisplay() {
       let actualCorps = this.controleur.getCorps();
